1a351006fe4201c632b14a8e158b1fda83b5dbb2
SANGAM KUMAR <ksangam990@gmail.com>
2026-08-12 21:07:06 +0530

    Update sendEmail.js

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -4,14 +4,14 @@ import nodemailer from 'nodemailer';
 
 export const sendEmail = async ({ to, subject, html }) => {
   const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    family: 4,
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
+       host: process.env.EMAIL_HOST,
+       port: process.env.EMAIL_PORT,
+       family: 4,
+       auth: {
+         user: process.env.EMAIL_USER,
+         pass: process.env.EMAIL_PASS,
+       },
+     });
 
   const mailOptions = {
     from: `Slotly <${process.env.EMAIL_FROM}>`,
